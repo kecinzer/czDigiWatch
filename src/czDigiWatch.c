3944e72e63cc5f55d71718c7acd17b2fba276543
kecinzer <kecinzer@gmail.com>
2015-05-25 14:11:37 +0200

    Added actual weather
    Based on pebble example
    
    Signed-off-by: kecinzer <kecinzer@gmail.com>

--- src/czDigiWatch.c
+++ src/czDigiWatch.c
@@ -8,6 +8,63 @@ static TextLayer *day_in_week;
 static TextLayer *date;
 static TextLayer *nameday1_line;
 static TextLayer *nameday2_line;
+static TextLayer *temperature_layer;
+static BitmapLayer *icon_layer;
+static GBitmap *icon_bitmap = NULL;
+
+static AppSync s_sync;
+static uint8_t s_sync_buffer[64];
+
+enum WeatherKey {
+  WEATHER_ICON_KEY = 0x0,         // TUPLE_INT
+  WEATHER_TEMPERATURE_KEY = 0x1,  // TUPLE_CSTRING
+};
+
+static const uint32_t WEATHER_ICONS[] = {
+  RESOURCE_ID_IMAGE_SUN, //0
+  RESOURCE_ID_IMAGE_CLOUD, //1
+  RESOURCE_ID_IMAGE_RAIN, //2
+  RESOURCE_ID_IMAGE_SNOW //3
+};
+
+static void sync_error_callback(DictionaryResult dict_error, AppMessageResult app_message_error, void *context) {
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "App Message Sync Error: %d", app_message_error);
+}
+
+static void sync_tuple_changed_callback(const uint32_t key, const Tuple* new_tuple, const Tuple* old_tuple, void* context) {
+  switch (key) {
+    case WEATHER_ICON_KEY:
+      if (icon_bitmap) {
+        gbitmap_destroy(icon_bitmap);
+      }
+
+      icon_bitmap = gbitmap_create_with_resource(WEATHER_ICONS[new_tuple->value->uint8]);
+      
+      bitmap_layer_set_bitmap(icon_layer, icon_bitmap);
+      break;
+
+    case WEATHER_TEMPERATURE_KEY:
+      // App Sync keeps new_tuple in s_sync_buffer, so we may use it directly
+      text_layer_set_text(temperature_layer, new_tuple->value->cstring);
+      break;
+  }
+}
+
+static void request_weather(void) {
+  DictionaryIterator *iter;
+  app_message_outbox_begin(&iter);
+
+  if (!iter) {
+    // Error creating outbound message
+    return;
+  }
+
+  int value = 1;
+  dict_write_int(iter, 1, &value, sizeof(int), true);
+  dict_write_end(iter);
+
+  app_message_outbox_send();
+}
 
 static void configureLayer(TextLayer *textlayer, const char *font, int alignment) {
   text_layer_set_font(textlayer, fonts_get_system_font(font));
@@ -48,7 +105,7 @@ static void update_time() {
   struct tm *t = localtime(&now);
 
   configureLayer(main_digits, FONT_KEY_BITHAM_42_BOLD, GTextAlignmentCenter);
-  configureLayer(seconds, FONT_KEY_GOTHIC_24_BOLD, GTextAlignmentCenter);
+  configureLayer(seconds, FONT_KEY_GOTHIC_24_BOLD, GTextAlignmentRight);
 
   static char digits[] = "00:00";
 
@@ -78,7 +135,7 @@ static void main_window_load(Window *window) {
   // Hodiny s minutami
   main_digits = text_layer_create(GRect(0, 0, 144, 50));
   // Sekundy
-  seconds = text_layer_create(GRect(0, 40, 144, 50));
+  seconds = text_layer_create(GRect(90, 40, 30, 25));
 
   update_time();
 
@@ -93,12 +150,30 @@ static void main_window_load(Window *window) {
   
   update_day();
 
+  icon_layer = bitmap_layer_create(GRect(26, 46, 20, 20));
+  temperature_layer = text_layer_create(GRect(40, 48, 50, 20));
+  configureLayer(temperature_layer, FONT_KEY_GOTHIC_14, GTextAlignmentCenter);
+  
+  Tuplet initial_values[] = {
+    TupletInteger(WEATHER_ICON_KEY, (uint8_t) 1),
+    TupletCString(WEATHER_TEMPERATURE_KEY, "1234\u00B0C"),
+  };
+
+  app_sync_init(&s_sync, s_sync_buffer, sizeof(s_sync_buffer), 
+      initial_values, ARRAY_LENGTH(initial_values),
+      sync_tuple_changed_callback, sync_error_callback, NULL
+  );
+
+  request_weather();
+
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(main_digits));
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(seconds));
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(day_in_week));
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(date));
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(nameday1_line));
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(nameday2_line));
+  layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(icon_layer));
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(temperature_layer));
 }
 
 static void main_window_unload(Window *window) {
@@ -109,6 +184,11 @@ static void main_window_unload(Window *window) {
   text_layer_destroy(date);
   text_layer_destroy(nameday1_line);
   text_layer_destroy(nameday2_line);
+  bitmap_layer_destroy(icon_layer);
+  text_layer_destroy(temperature_layer);
+  if (icon_bitmap) {
+    gbitmap_destroy(icon_bitmap);
+  }
 }
 
 static void refresh_every_second(struct tm *tick_time, TimeUnits units_changed) {
@@ -129,6 +209,8 @@ static void init() {
   window_stack_push(s_main_window, true);
   // Registrace sekundového sledování
   tick_timer_service_subscribe(SECOND_UNIT, refresh_every_second);
+  // Otevření komunikace s JS
+  app_message_open(64, 64);
 }
 
 static void deinit() {
